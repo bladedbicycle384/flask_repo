2c49e72ccaaaaac4b206ff9edff76578eb5c35b4
Mihail Dimitrov <36842438+bladedbicycle384@users.noreply.github.com>
2020-12-15 08:13:58 +0200

    Update comment.py

--- comment.py
+++ comment.py
@@ -1,19 +1,43 @@
 from database import DB
+from comment import Comment
 
-class Comment:
-    def __init__(self, comment_id, post, message):
-        self.comment_id = comment_id
-        self.post = post
-        self.message = message
+
+class Post:
+    def __init__(self, post_id, name, author, content):
+        self.post_id = post_id
+        self.name = name
+        self.author = author
+        self.content = content
+
+    @staticmethod
+    def all():
+        with DB() as db:
+            rows = db.execute('SELECT * FROM posts').fetchall()
+            return [Post(*row) for row in rows]
+
+    @staticmethod
+    def find(post_id):
+        with DB() as db:
+            row = db.execute('SELECT * FROM posts WHERE post_id = ?', (post_id,)).fetchone()
+            if row is None:
+                return
+            return Post(*row)
 
     def create(self):
         with DB() as db:
-            values = (self.post.post_id, self.message)
-            db.execute('INSERT INTO comments (post_id, message) VALUES (?, ?)', values)
+            values = (self.name, self.author, self.content)
+            row = db.execute('INSERT INTO posts (name, author, content) VALUES (?, ?, ?)', values)
             return self
 
-    @staticmethod
-    def find_by_post(post):
+    def save(self):
         with DB() as db:
-            rows = db.execute('SELECT * FROM comments WHERE post_id = ?', (post.post_id,)).fetchall()
-            return [Comment(*row) for row in rows]
+            values = (self.name, self.author, self.content, self.post_id)
+            db.execute('UPDATE posts SET name = ?, author = ?, content = ? WHERE post_id = ?', values)
+            return self
+
+    def delete(self):
+        with DB() as db:
+            db.execute('DELETE FROM posts WHERE post_id = ?', (self.post_id,))
+
+    def comments(self):
+        return Comment.find_by_post(self)
